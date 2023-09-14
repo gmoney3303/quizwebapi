526f16258bc6bf24fe8e54e2e80ce9a07f2f9e39
garrett sheppard <garrettmsheppard@yahoo.com>
2023-09-13 18:16:16 -0600

    starting code

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var correct = document.getElementById("fourth-answer");
+var wrong = document.getElementById("first-answer", "second-answer", "third-answer");
+
+correct.addEventListener("click", function(event){
+    
+})
